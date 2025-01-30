79d166b2efd5f9817d82a84ca05531427e25ebe6
Nikita <savvin.nikit@yandex.ru>
2025-01-30 17:06:50 +0300

    save

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /var/lib/grafana/.initialized ]; then
-  echo "Initializing Grafana..."
-  touch /var/lib/grafana/.initialized
-fi
-
-echo "Starting main service..."
-exec "$@"
